369bd96233cec84754ecafd3678d6621809cbcaf
its-kevin228 <pekpelignimdoukevin@gmail.com>
2025-08-27 11:58:28 +0000

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import HOME from './pages/HOME.JSX'
+import Blog from './pages/Blog'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<HOME/>} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
     </div>
   )
 }
---
+++ client/src/pages/HOME.jsx
@@ -0,0 +1,12 @@
+
+import React from "react"
+import Navbar from "../components/Navbar"
+const HOME = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default HOME
