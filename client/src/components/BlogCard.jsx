febfb5d016e00db7c6b7a6babee995a631973325
its-kevin228 <pekpelignimdoukevin@gmail.com>
2025-08-27 14:29:05 +0000

    bloglist and his integration

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -6,16 +6,14 @@ const {title, description, image, _id, category} = blog;
 const navigate = useNavigate();
 
   return (
-    <div onClick={() => navigate(`/blog/${_id}`)} className='w-full rounded-full overflow-hidden shadow hover:scale-102 hover:shadow-primary/25 duration-300 cursor-pointer'>
+    <div onClick={() => navigate(`/blog/${_id}`)} className='w-full rounded-lg overflow-hidden shadow hover:scale-102 hover:shadow-primary/25 duration-300 cursor-pointer'>
       <img src={image} alt={title} className='aspect-video' />
       <span className='ml-5 mt-4 px-3 py-1 inline-block bg-primary/20 rounded-full text-primary text-xs'>{category}</span>
       <div className='p-5'>
         <h5 className='mb-2 font-medium text-gray-900 '>
             {title}
         </h5>
-        <p className=' mb-3 text-xs text-gray-600 '>
-            {description.slice(0, 80)}
-        </p>
+        <p className=' mb-3 text-xs text-gray-600 ' dangerouslySetInnerHTML={{ __html: description.slice(0, 80) }} />
       </div>
     </div>
   )
